a70d818e3e6a2e74a901bb0946dd5d9a5f899afa
JunHee Kim <junhui4835@naver.com>
2024-03-18 20:09:42 +0900

    2024-03-18-20:09

--- mySite/settings.py
+++ mySite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', #polls\apps 의 config 참조
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
